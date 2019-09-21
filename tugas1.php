a366c9ec5ec491d973b6cd64abbfab5754191655
andinikidd <55625016+andinikidd@users.noreply.github.com>
2019-09-21 21:51:25 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,42 @@
+<?php
+
+$a = 10;
+$t = 15;
+
+echo "Luas Segitiga adalah = 2 x (a x t / 2)";
+echo "<br>";
+echo "Alas =". $a;
+echo "<br>";
+echo "Tinggi =". $t;
+echo "<br>";
+echo "Luas Segitiga adalah : ". 2*($a*$t/2);
+
+echo "<br>";
+echo "<br>";
+
+$x = 22/7;
+$y = 14;
+
+echo "Luas Lingkaran adalah = π × r²";
+echo "<br>";
+echo "π =". $x;
+echo "<br>";
+echo "Jari-jari =". $y;
+echo "<br>";
+echo "Luas Lingkaran adalah : ". $x*$y*$y;
+
+echo "<br>";
+echo "<br>";
+
+$b = 24;
+$c = 7;
+
+echo "Luas Jajar Genjang adalah = a x t";
+echo "<br>";
+echo "Alas =". $b;
+echo "<br>";
+echo "Tinggi =". $c;
+echo "<br>";
+echo "Luas Jajar Genjang adalah :". $b*$c;
+
+?>
